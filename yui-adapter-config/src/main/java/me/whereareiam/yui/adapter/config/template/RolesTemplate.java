67e40ad5e6b00dde2b93f623379d08e957d51556
whereareiam <mmm.g032@gmail.com>
2025-08-18 18:33:25 +0200

    fix(config): instead of setting permanent values into map, we should let it be empty

--- yui-adapter-config/src/main/java/me/whereareiam/yui/adapter/config/template/RolesTemplate.java
+++ yui-adapter-config/src/main/java/me/whereareiam/yui/adapter/config/template/RolesTemplate.java
@@ -2,7 +2,6 @@ package me.whereareiam.yui.adapter.config.template;
 
 import me.whereareiam.yui.api.model.config.Roles;
 import me.whereareiam.yui.api.output.config.DefaultConfig;
-import net.dv8tion.jda.api.interactions.DiscordLocale;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -14,18 +13,8 @@ public class RolesTemplate implements DefaultConfig<Roles> {
 		Roles roles = new Roles();
 
 		// Default values
-		roles.setLanguageRoles(Map.of(
-				DiscordLocale.ENGLISH_US.getLocale(), 1206929604971859978L,
-				DiscordLocale.GERMAN.getLocale(), 1206929557987401740L,
-				DiscordLocale.RUSSIAN.getLocale(), 1206929308355137597L
-		));
-
-		roles.setAllowedRoles(Map.of(
-				"us", 1206929604971859978L,
-				"de", 1206929557987401740L,
-				"ru", 1206929308355137597L,
-				"verified", 856952987896774696L
-		));
+		roles.setLanguageRoles(Map.of());
+		roles.setAllowedRoles(Map.of());
 
 		return roles;
 	}
