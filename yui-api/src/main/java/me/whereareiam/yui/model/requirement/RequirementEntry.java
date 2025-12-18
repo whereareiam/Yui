74dd68452bf6a022a05c3dad8185239e5775338c
whereareiam <mmm.g032@gmail.com>
2025-12-18 17:08:13 +0100

    fix: use polymorphic for requirements

--- yui-api/src/main/java/me/whereareiam/yui/model/requirement/RequirementEntry.java
+++ yui-api/src/main/java/me/whereareiam/yui/model/requirement/RequirementEntry.java
@@ -2,6 +2,12 @@ package me.whereareiam.yui.model.requirement;
 
 import lombok.Getter;
 import lombok.Setter;
+import me.whereareiam.configura.annotation.Polymorphic;
+import me.whereareiam.yui.model.requirement.type.ChannelTypeRequirement;
+import me.whereareiam.yui.model.requirement.type.GuildRequirement;
+import me.whereareiam.yui.model.requirement.type.RoleRequirement;
+import me.whereareiam.yui.model.requirement.type.ScopeRequirement;
+import me.whereareiam.yui.model.requirement.type.UserRequirement;
 import me.whereareiam.yui.type.requirement.RequirementCondition;
 
 /**
@@ -10,6 +16,15 @@ import me.whereareiam.yui.type.requirement.RequirementCondition;
  */
 @Getter
 @Setter
+@Polymorphic(
+		inferBy = {
+				@Polymorphic.Infer(field = "roles", target = RoleRequirement.class),
+				@Polymorphic.Infer(field = "userIds", target = UserRequirement.class),
+				@Polymorphic.Infer(field = "guildIds", target = GuildRequirement.class),
+				@Polymorphic.Infer(field = "types", target = ChannelTypeRequirement.class),
+				@Polymorphic.Infer(field = "scopes", target = ScopeRequirement.class)
+		}
+)
 public abstract class RequirementEntry {
 	private RequirementCondition condition;
 	private Boolean expected; // default true when null
